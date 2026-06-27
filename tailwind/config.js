82360e39aaa48d5be7db44dc828454e7e64b889c
zetoroz <zetoroz@gmail.com>
2026-06-27 21:01:16 +0800

    SEO perf+EEAT: compile Tailwind to static 18KB CSS (drop CDN), Person author (Anto Jose) sitewide, image width/height

---
+++ tailwind/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./index.html", "./guides/**/*.html"],
+  theme: { extend: {
+    colors: { cream:'#F7F4EF', paper:'#FCFAF6', ink:'#15233B', navy:'#1B2D49', clay:'#C26A3C', 'clay-deep':'#A9542B', muted:'#5B6472', line:'#E6E0D6' },
+    fontFamily: { display:['Fraunces','serif'], sans:['Manrope','system-ui','sans-serif'] },
+    maxWidth: { content:'76rem' },
+  } }
+}
